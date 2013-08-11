fb5b69f29b459b57f97b8d99a70c3763eba7fc0b
Michael Garate <mpgarate@gmail.com>
2013-08-11 16:25:07 -0400

    added favicon

--- dev/index.php
+++ dev/index.php
@@ -2,13 +2,14 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-		
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="The official release website for the DA/TES by Splotch and Emoticon Dream." />
 		<meta name="keywords" content="DA/TES daytes dates d8ts da8s collin peterson michael garate emoticon dream splotch"/>
 		<title>DA/TES</title>
+
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	<link rel="icon" href="favicon.ico" type="image/x-icon">
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
@@ -52,18 +53,18 @@
 				<div class="hidden-sm boxes col-lg-4 col-sm-4 boxes-left"></div>
 			</div>
 
-<div class="row social-row">
-	<div class="col-4 col-lg-offset-5 ">
-		<!-- AddThis Button BEGIN -->
-		<div class="addthis_toolbox addthis_default_style ">
-		<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
-		<a class="addthis_button_tweet"></a>
-		</div>
-		<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5202e0eb332120df"></script>
-		<!-- AddThis Button END -->
-	</div>
-</div>
+			<div class="row social-row">
+				<div class="col-4 col-lg-offset-5 ">
+					<!-- AddThis Button BEGIN -->
+					<div class="addthis_toolbox addthis_default_style ">
+					<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+					<a class="addthis_button_tweet"></a>
+					</div>
+					<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+					<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5202e0eb332120df"></script>
+					<!-- AddThis Button END -->
+				</div>
+			</div>
 
     </div> <!-- /container -->
 
@@ -74,5 +75,16 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
     <script src="js/playergrid.js"></script>
+    <!-- Google Analytics start -->
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-2515841-14', 'emoticondream.com');
+	  ga('send', 'pageview');
+	</script>
+    <!-- Google Analytics end -->
   </body>
 </html>
