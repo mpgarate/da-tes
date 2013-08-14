4703ab0db3c38924d6f3b3982b5d357afa71fd5c
Michael Garate <mpgarate@gmail.com>
2013-08-14 07:19:28 -0400

    restructured directories for launch

---
+++ dates.php
@@ -0,0 +1,47 @@
+<?php
+	function printDate($date, $class, $title, $content, $place="auto"){ 
+		echo "<div class='date-holder " . $class . " col-6 col-sm-2 col-lg-2'>";
+		echo "<a href='#'
+			title='" . $title . "'
+			data-placement='" . $place . "'
+			data-content='" . $content . "'>
+			" . $date . "
+		</a></div>";
+	}
+?>
+<div class="row dates dates-1">
+	<div class="hidden-sm col-1"></div>
+	<?php printDate("08-12-77","y-77","August 12, 1877","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("12-31-62","y-62","December 31, 1962","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("05-06-56","y-56","May 6, 1956","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("01-10-92","y-92","January 10, 1892","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("11-06-52","y-52","November 6, 1852","lorem ipsum dolor sit amet"); ?>
+	<div class="hidden-sm col-1"></div>
+</div> <!-- .row .dates-1 -->
+<div class="row dates dates-2">
+	<div class="hidden-sm col-1"></div>
+	<?php printDate("12-31-62","y-62","December 31, 1962","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("05-06-56","y-56","May 6, 1956","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("01-10-92","y-92","January 10, 1892","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("11-06-52","y-52","November 6, 1852","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("08-12-77","y-77","August 12, 1877","lorem ipsum dolor sit amet"); ?>
+	<div class="hidden-sm col-1"></div>
+</div> <!-- .row .dates-2 -->
+<div class="hidden-sm row dates dates-3">
+	<div class="hidden-sm col-1"></div>
+	<?php printDate("05-06-56","y-56","May 6, 1956","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("01-10-92","y-92","January 10, 1892","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("11-06-52","y-52","November 6, 1852","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("08-12-77","y-77","August 12, 1877","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("12-31-62","y-62","December 31, 1962","lorem ipsum dolor sit amet"); ?>
+	<div class="hidden-sm col-1"></div>
+</div> <!-- .row .dates-3 -->
+<div class="hidden-sm row dates dates-4">
+	<div class="hidden-sm col-1"></div>
+	<?php printDate("01-10-92","y-92","January 10, 1892","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("11-06-52","y-52","November 6, 1852","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("08-12-77","y-77","August 12, 1877","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("12-31-62","y-62","December 31, 1962","lorem ipsum dolor sit amet"); ?>
+	<?php printDate("05-06-56","y-56","May 6, 1956","lorem ipsum dolor sit amet"); ?>
+	<div class="col-1"></div>
+</div> <!-- .row .dates-4 -->
